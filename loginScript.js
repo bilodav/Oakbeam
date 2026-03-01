1a3483edf52c7c69c718839630c10a7d6c294bc7
bilodav <bilodavids@gmail.com>
2026-03-01 14:24:34 +0200

    Added somme more functiionality and styling to the login form

--- loginScript.js
+++ loginScript.js
@@ -1,8 +1,10 @@
 // Login page animation
 
 const loginSignUpBtn = document.querySelector(".login-sign-up");
-const loginCover = document.querySelector(".login-cover")
-const loginHeading = document.querySelector(".login-cover h2")
+const loginCover = document.querySelector(".login-cover");
+const loginHeading = document.querySelector(".login-cover h2");
+const loginText = document.querySelector(".login-cover-text")
+const loginHomeBtn  = document.querySelector(".login-home-btn");
 
 loginSignUpBtn.addEventListener("click", () => {
   const coverContent = loginCover.querySelectorAll("h2, button");
@@ -15,8 +17,10 @@ loginSignUpBtn.addEventListener("click", () => {
     loginCover.classList.remove("login-cover-right");
 
     setTimeout(() => {
+        
       loginSignUpBtn.textContent = "Sign In";
-      loginHeading.textContent = "Already part of us? Sign in friend";
+      loginHeading.textContent = "Welcome Back!";
+      loginText.textContent = "Enter your personal details to use all of the site features"
       coverContent.forEach(el => el.style.opacity = "1");
     }, 300); // fade back in mid-transition
 
@@ -26,8 +30,13 @@ loginSignUpBtn.addEventListener("click", () => {
 
     setTimeout(() => {
       loginSignUpBtn.textContent = "Sign Up";
-      loginHeading.textContent = "New to us? Sign Up Now";
+      loginHeading.textContent = "Hello, Friend!";
+      loginText.textContent = "Register with your personal details to use all of the site features";
       coverContent.forEach(el => el.style.opacity = "1");
     }, 300);
   }
 });
+
+loginHomeBtn.addEventListener("click", ()=>{
+    window.location.href =  "./index.html"
+})
